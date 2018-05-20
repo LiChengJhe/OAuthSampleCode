505ceddfbd3dff61fc267497e5fa043819a6e16f
Li,Cheng-Jhe <chengjhe810824@gmail.com>
2018-05-20 22:21:13 +0800

    fixed polyfills.ts
    Signed-off-by: Li,Cheng-Jhe <chengjhe810824@gmail.com>

--- Website/src/app/layout/layout-routing.module.ts
+++ Website/src/app/layout/layout-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/Authentication/Oauth2Client', pathMatch: 'full' },
-  { path: '**', redirectTo: '/Authentication/Oauth2Client', pathMatch: 'full' }
+  { path: '', redirectTo: '/Authentication/Oauth2Client', pathMatch: 'full' }
 ];
 
 @NgModule({
